178fa2067370c5b0f7bd7d5172e5a3bea5d85df6
zoltan23 <timothy_tassin@hotmail.com>
2023-05-01 18:58:30 -0600

    add core directory

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,14 @@
+"""
+Django command that waits for the db to be available.
+"""
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """
+    Django command to wait for database.
+    """
+
+    def handle(self, *args, **options):
+        pass
